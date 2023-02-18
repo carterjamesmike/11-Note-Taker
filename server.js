35433ea968782bf01fae60e7829281bf8813fe82
James Carter <carterjamesmike@gmail.com>
2023-02-17 19:28:47 -0600

    Initial GET route for notes complte. Route structure complete

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+const { notes } = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//app.use('/api', api)
+
+app.use(express.static('public'));
+
+//API Routes
+//GET homepage
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//GET /api/notes
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+//POST /api/notes
+
+//DELETE /api/notes/:id
 
 
 
